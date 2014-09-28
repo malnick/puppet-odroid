951b9137b8e0debd84dda079129d20d0cf8e0a64
Jeff <malnick@gmail.com>
2014-09-27 20:38:15 -0700

    fixed

--- odroid/manifests/network.pp
+++ odroid/manifests/network.pp
@@ -3,8 +3,11 @@ class odroid::network(){
   file{'/etc/network/interfaces':
     ensure => file,
     content => template('odroid/interfaces'),
+    notify => Service['networking']
   }
 
-
-
+  service{'networking':
+    ensure => running,
+    enable => true,
+  }
 }
