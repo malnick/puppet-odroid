06c214acff45506b98b86309b34bd25657ab1900
Jeff <malnick@gmail.com>
2014-09-27 23:14:28 -0700

    rcd will gcc on open port each time

--- odroid/manifests/rc_d.pp
+++ odroid/manifests/rc_d.pp
@@ -20,7 +20,7 @@ class odroid::rc_d (){
   exec{'port_open':
     command => '/usr/bin/gcc -o /etc/init.d/port_open /etc/init.d/port_open.c',
     require => File['/etc/init.d/port_open.c'],
-    creates => '/etc/init.d/port_open',
+    #creates => '/etc/init.d/port_open',
   }
 
   service{'status.sh':
